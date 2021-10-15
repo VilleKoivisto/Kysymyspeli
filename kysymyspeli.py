08826bf987861f3c30bde74675f2465c805435bc
jphieno <jari-pekka.heino@outlook.com>
2021-10-15 14:41:48 +0300

    created my version of a game and committed it

--- kysymyspeli.py
+++ kysymyspeli.py
@@ -1,7 +1,48 @@
 # Tästä lähtee meidän superhieno kyssäripeli
 
 def main():
-    pass
+    laskuri = 0
+
+    print("Let the games begin")
+    while True:
+        kysymys1 = input("Kissa vai Koira? (1) = Kissa, (2) = Koira: ")
+        if kysymys1 == "1":
+            print("Väärä vastaus :(")
+        elif kysymys1 == "2":
+            print("Oikea vastaus!!!!")
+            laskuri += 1
+        else:
+            print("Valitse numero 1 tai 2")
+
+        kysymys2 = input("Vasen vai Oikea? (1) = Vasen, (2) = Oikea: ")
+        if kysymys2 == "1":
+            print("Väärä vastaus :(")
+        elif kysymys2 == "2":
+            print("Oikea vastaus!!!!")
+            laskuri += 1
+        else:
+            print("Valitse numero 1 tai 2")
+
+        kysymys3 = input("Salaatti vai Pitsa? (1) = Salaatti, (2) = Pitsa: ")
+        if kysymys3 == "1":
+            print("Väärä vastaus :(")
+        elif kysymys3 == "2":
+            print("Oikea vastaus!!!!")
+            laskuri += 1
+        else:
+            print("Valitse numero 1 tai 2")
+        break
+    print(f"Kokonaispistemääräsi on {laskuri} pistettä!")
+    if laskuri == 3:
+        print("Täydet pisteet, avaa olut hyvillä mielin!")
+    elif laskuri == 2:
+        print("Hyvät pojot, juo puolikas olut")
+    elif laskuri == 1:
+        print("Pystyt parempaan!")
+    else:
+        print("daaaaaaamn...")
+
+
 
 
 if __name__ == "__main__":
