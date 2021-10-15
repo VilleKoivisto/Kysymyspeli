9aaa23f4234867a6378caae9d43c7ca6e05f1462
Kirsi <kirsi.r.holmberg@gmail.com>
2021-10-15 14:47:11 +0300

    Kirsin ensimmäinen versio kysymyspelistä

--- kysymyspeli.py
+++ kysymyspeli.py
@@ -1,7 +1,30 @@
 # Tästä lähtee meidän superhieno kyssäripeli
 
 def main():
-    pass
+    index = 1
+    pisteet = 0
+    while index < 3:
+        vastaus = input(print("Voiko Google Cloud SDKn asentaa PClle ? k=kyllä, e=ei"))
+        if vastaus == "k" or vastaus == "K":
+            pisteet += 1
+        else:
+            print("Ei pistetä")
+        index += 1
+        vastaus = input(print("Luotko virtuaali koneet VM instancessa ? k=kyllä, e=ei"))
+        if vastaus == "k" or vastaus == "K":
+            pisteet += 1
+        else:
+            print("Ei pistetä")
+        index += 1
+        vastaus = input(print("Oliko ensimmäinen Checkpoin vaikea ? k=kyllä, e=ei"))
+        if vastaus == "e" or vastaus == "E":
+            pisteet += 1
+        else:
+            print("Ei pistetä")
+        index += 1
+    print(f"Sain näin paljon pisteitä (max 3 pistettä) {pisteet}")
+        
+
 
 
 if __name__ == "__main__":
